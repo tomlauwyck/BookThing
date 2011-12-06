8cfeb78efe6fad7e758b81ec533650ac08bcf131
tomlauwyck <tom.lauwyck@gmail.com>
2011-12-06 18:45:45 +0100

    second initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookThing::Application.config.secret_token = 'c6dce18da972265ef04c42d0136ce4edbdf069ada22eed3a4ba24d4c7a60617f70f7873a6870f6b595d2dbb8833d801ec2d49c8f37413d738a64494488ba9dce'
