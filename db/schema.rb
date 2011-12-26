023310626aecf23f526f955f5d00f07f8e652984
tomlauwyck <tom.lauwyck@gmail.com>
2011-12-26 15:49:18 +0100

    Finished first part of user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111206195116) do
-
-  create_table "books", :force => true do |t|
-    t.string   "title"
-    t.string   "author"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(:version => 20111226143204) do
 
   create_table "users", :force => true do |t|
+    t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
